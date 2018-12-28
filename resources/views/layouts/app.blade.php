6cabd262d6a2b40fc0545b702104dd2a4253c00d
Darren <darren@jacoby.co.za>
2018-12-28 16:11:21 +0200

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,11 +9,6 @@
         <main class="main">
           @yield('content')
         </main>
-        @if (App\display_sidebar())
-          <aside class="sidebar">
-            @include('partials.sidebar')
-          </aside>
-        @endif
       </div>
     </div>
     @php do_action('get_footer') @endphp
