74d1033bd918fdd6b18e5dbb20825753d951f11e
Darren <darren@jacoby.co.za>
2018-12-29 14:00:58 +0200

    Polyfills

--- app/polyfills.php
+++ app/polyfills.php
@@ -6,6 +6,10 @@ namespace App;
  * Theme assets
  */
 add_action('wp_enqueue_scripts', function () {
-    // add Promise for IE11
+    // add Promise,Element.prototype.classList for IE11
     wp_enqueue_script('polyfill', 'https://polyfill.io/v2/polyfill.min.js?features=IntersectionObserver,Array.from', false, null, true);
+
+    // IE11 object-fit polyfill
+    // https://github.com/bfred-it/object-fit-images/
+    // wp_enqueue_script('object-fit', 'https://cdnjs.cloudflare.com/ajax/libs/object-fit-images/3.2.4/ofi.min.js', false, null, true);
 }, 50);
