8ca9595d35e1d2a799308930a1e7c1ba7edd31d3
Darren <darren@jacoby.co.za>
2018-12-30 10:28:40 +0200

    Polyfills

--- app/polyfills.php
+++ app/polyfills.php
@@ -13,6 +13,8 @@ add_action('wp_enqueue_scripts', function () {
     wp_enqueue_script('polyfill', 'https://polyfill.io/v2/polyfill.min.js?features=IntersectionObserver,Array.from', false, null, true);
 
     // IE11
-    // wp_enqueue_script('polyfill', 'https://polyfill.io/v2/polyfill.min.js?features=IntersectionObserver,Array.from,Promise,Element.prototype.classList', false, null, true);
-    // wp_enqueue_script('object-fit', 'https://cdnjs.cloudflare.com/ajax/libs/object-fit-images/3.2.4/ofi.min.js', false, null, true);
+    /**
+     * wp_enqueue_script('polyfill', 'https://polyfill.io/v2/polyfill.min.js?features=IntersectionObserver,Array.from,Promise,Element.prototype.classList', false, null, true);
+     * wp_enqueue_script('object-fit', 'https://cdnjs.cloudflare.com/ajax/libs/object-fit-images/3.2.4/ofi.min.js', false, null, true);
+     */
 }, 50);
