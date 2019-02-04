7eb160445b927eebeaf1c8f8ee8b59ff5ad15fcb
Darren <darren@jacoby.co.za>
2019-02-04 21:53:47 +0200

    Trellis for API keys

--- app/config.php
+++ app/config.php
@@ -82,7 +82,6 @@ add_filter('sober/models/path', function () use ($post_types) {
  * @link https://developers.google.com/maps/documentation/javascript/get-api-key
  */
 $acf_json = locate_config('acf-json');
-$api_keys = include locate_config('api-keys');
 
 add_filter('acf/settings/save_json', function ($path) use ($acf_json) {
     return $acf_json;
@@ -90,8 +89,4 @@ add_filter('acf/settings/save_json', function ($path) use ($acf_json) {
 
 add_filter('acf/settings/load_json', function ($path) use ($acf_json) {
     return [$acf_json];
-});
-
-add_action('acf/init', function () use ($api_keys) {
-    acf_update_setting('google_api_key', $api_keys['google']);
 });
